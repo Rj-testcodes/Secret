65d8196aaa682c278aad3bc321d04f4d57894f14
RJ <bubby101110@gmail.com>
2026-09-22 13:11:58 -0400

    Fix class declaration in SecretClient.java

--- src/client/java/ABC/secret/client/SecretClient.java
+++ src/client/java/ABC/secret/client/SecretClient.java
@@ -7,7 +7,7 @@ public class SecretClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
 
-public class Test implements 
+public class Test
         
     }
 }
