ffc55da6bb23253ac6112fc8bc798edb9fef68b0
Rj-testcodes <bubby101110@gmail.com>
2026-09-22 16:09:58 -0400

    update

--- src/main/java/ABC/secret/Secret.java
+++ src/main/java/ABC/secret/Secret.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 public class Secret implements ModInitializer {
 
-    private static final Logger LOGGER
+
 
     @Override
     public void onInitialize() {
