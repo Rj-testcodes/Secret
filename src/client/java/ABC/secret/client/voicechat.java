e03d5d561da87e61ac21cc117b1c66fafee3292f
RJ <bubby101110@gmail.com>
2026-09-22 07:46:12 -0400

    Create voicechat.java

---
+++ src/client/java/ABC/secret/client/voicechat.java
@@ -0,0 +1,13 @@
+package ABC.secret.voicechat;
+
+import de.maxhenkel.voicechat.api.*;
+import de.maxhenkel.voicechat.api.events.EventRegistration;
+import de.maxhenkel.voicechat.api.events.MicrophonePacketEvent;
+import de.maxhenkel.voicechat.api.opus.OpusDecoder;
+import de.maxhenkel.voicechat.api.opus.OpusEncoder;
+import de.maxhenkel.voicechat.api.packets.LocationalSoundPacket;
+import de.maxhenkel.voicechat.api.packets.MicrophonePacket;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.math.Vec3d;
+
+import java.util.UUID;
