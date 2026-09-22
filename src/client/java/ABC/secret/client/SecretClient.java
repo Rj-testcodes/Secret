d15f0a2549b3a3045f6887f5f65d00304bca4ee3
Rj-testcodes <bubby101110@gmail.com>
2026-09-21 21:38:04 -0400

    Initial commit

---
+++ src/client/java/ABC/secret/client/SecretClient.java
@@ -0,0 +1,10 @@
+package ABC.secret.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SecretClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/ABC/secret/Secret.java
@@ -0,0 +1,10 @@
+package ABC.secret;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Secret implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
